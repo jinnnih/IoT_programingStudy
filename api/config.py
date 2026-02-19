9eec40c9624c7f16e5354585bb4bc981982d9e8d
함현진 <jinnnih@gmail.com>
2026-02-19 15:21:04 +0900

    new 2단계 회원가입 엔드포인트 DB연동

--- api/config.py
+++ api/config.py
@@ -1,11 +1,9 @@
 db = {
-    'user'     : 'iotuser',
-    'password' : '1234',
-    'host'     : 'localhost',
-    'port'     : 3306,
-    'database' : 'miniter'
+   'user'     : 'root',
+   'password' : 'test1234',
+   'host'     : 'localhost',
+   'port'     : 3306,
+   'database' : 'miniter'
 }
 
 DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
-
-
